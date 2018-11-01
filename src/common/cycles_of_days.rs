ff1cec2b350fa631a906167871b77365cc70ca15
Yosef Deray <yosefderay770@gmail.com>
2018-10-31 20:04:34 -0400

    changed return from cycle of day functions that return R.D. dates to RD

--- src/common/cycles_of_days.rs
+++ src/common/cycles_of_days.rs
@@ -4,27 +4,27 @@ pub fn day_of_week_from_fixed(date: RD) -> i32 {
     divide(date, 7).1
 }
 
-pub fn kday_on_or_before(date: RD, k: i32) -> i32 {
+pub fn kday_on_or_before(date: RD, k: i32) -> RD {
     date - day_of_week_from_fixed(date - k)
 }
 
-pub fn kday_on_or_after(date: RD, k: i32) -> i32 {
+pub fn kday_on_or_after(date: RD, k: i32) -> RD {
     kday_on_or_before(date + 6, k)
 }
 
-pub fn kday_nearest(date: RD, k: i32) -> i32 {
+pub fn kday_nearest(date: RD, k: i32) -> RD {
     kday_on_or_before(date + 3, k)
 }
 
-pub fn kday_before(date: RD, k: i32) -> i32 {
+pub fn kday_before(date: RD, k: i32) -> RD {
     kday_on_or_before(date - 1, k)
 }
 
-pub fn kday_after(date: RD, k: i32) -> i32 {
+pub fn kday_after(date: RD, k: i32) -> RD {
     kday_on_or_before(date + 7, k)
 }
 
-pub fn nth_kday(date: RD, n: i32, k: i32) -> i32 {
+pub fn nth_kday(date: RD, n: i32, k: i32) -> RD {
     7 * n + if n > 0 {
         kday_before(date, k)
     } else {
